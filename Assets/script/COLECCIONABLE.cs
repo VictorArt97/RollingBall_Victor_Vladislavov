586a5739e13911f1617983808a53c6011cf0784d
VictorArt97 <Bakalovvictor@gmail.com>
2024-09-30 17:51:40 +0200

    30/09/24
    traslación y rotación

---
+++ Assets/script/COLECCIONABLE.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COLECCIONABLE : MonoBehaviour
+{
+    [SerializeField] Vector3 sentido;
+    [SerializeField] int velodidadRotacion;
+    [SerializeField] Vector3 direccion ;
+ 
+    [SerializeField] int velocidad;
+    float timer;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+        transform.Rotate(sentido*velodidadRotacion*Time.deltaTime,Space.World); // , Space.World para que gire en torno al mundo
+        if (timer >= 2)
+        {
+
+            timer = 0;
+            direccion *= -1;
+           
+        }
+    }
+}
---
+++ Assets/script/Plataforma.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    [SerializeField] Vector3 direccion ;
+ 
+    [SerializeField] int velocidad;
+    float timer;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        timer += 1 * Time.deltaTime;
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+        if (timer >=5)
+        {
+           
+            timer = 0;
+            direccion *= -1;
+        }
+    }
+}
